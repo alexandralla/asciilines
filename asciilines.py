7c203802616fba0c4a0ac6445df268a5d067b069
Alexandra Llamas <allamas@augusta.cs.pdx.edu>
2019-06-30 14:44:30 -0700

    Added working 2d dot array implementation

--- asciilines.py
+++ asciilines.py
@@ -1,9 +1,45 @@
 import fileinput
-  
-lineNumber = 0;
+import numpy as np
 
-for line in fileinput.input():
-    print(line)
-    lineNumber += 1;
+inFile = fileinput.input();
 
-print('the size of this file is', lineNumber)
+
+def main():
+
+    lineNumber = 0
+    width = 0
+    height = 0
+
+    for line in inFile:
+
+        if (lineNumber == 0):
+
+            size = line.split(" ")
+            width = int(size[0])
+            height = int(size[1])
+
+            buildCanvas(width, height);
+
+
+#        if (lineNumber == 1):
+#            print('in line 1')
+#
+#        if (lineNumber == 2):
+#            print('in line 2')
+#
+        lineNumber += 1
+
+def buildCanvas(width, height):
+    
+    canvas = np.zeros([width, height]);
+
+    for i in range(width):
+        for j in range(height):
+            print('.', end=' ')
+        print()
+
+#    print(canvas)
+
+
+if __name__ == "__main__":
+    main()
