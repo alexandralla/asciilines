162baa1746154b846d7e7282563b0b2e03e0c0a0
Alexandra Llamas <allamas@augusta.cs.pdx.edu>
2019-06-30 21:51:49 -0700

    Added working implementation of line rendering. Also added a test2 instance that tests a bigger area.

--- asciilines.py
+++ asciilines.py
@@ -1,14 +1,15 @@
 import fileinput
-import numpy as np
 
-inFile = fileinput.input();
+inFile = fileinput.input()
+width = 0
+height = 0
 
 
 def main():
 
     lineNumber = 0
-    width = 0
-    height = 0
+    global width
+    global height
 
     for line in inFile:
 
@@ -18,27 +19,102 @@ def main():
             width = int(size[0])
             height = int(size[1])
 
-            buildCanvas(width, height);
+            canvas = buildCanvas(width, height)
+            lineNumber += 1
 
+        else:
 
-#        if (lineNumber == 1):
-#            print('in line 1')
-#
-#        if (lineNumber == 2):
-#            print('in line 2')
-#
-        lineNumber += 1
+            render = line.split(" ")
+            char = render[0]
+            rowPos = int(render[1])
+            colPos = int(render[2])
+            direction = render[3]
+            length = int(render[4])
+
+            canvas = renderLine(char, rowPos, colPos, direction, length, canvas)
+
+    show(canvas)
+
 
 def buildCanvas(width, height):
     
-    canvas = np.zeros([width, height]);
+    canvas = [[0 for x in range(height)] for y in range(width)] 
+
+    print('\nOriginal canvas:')
+    for i in range(width):
+        for j in range(height):
+            canvas[i][j] = '.'
+            print(canvas[i][j], end=' ')
+        print()
+
+
+    return canvas
+
+def renderLine(char, rowPos, colPos, direction, length, canvas):
+
+    count = 0
+#    print('\nRendering new line:')
+
+
+    if (rowPos < 0):
+        rowPos = 0
+
+    if (rowPos > height):
+        rowPos = height
+
+    if (colPos < 0):
+        colPos = 0
+
+    if (colPos > width):
+        colPos = width
+
+    if (length < 0):
+        length = 0
+
+    if (direction == 'h' and length > width):
+        length = width
+
+    if (direction == 'v' and length > height):
+        length = height
 
     for i in range(width):
         for j in range(height):
-            print('.', end=' ')
-        print()
+            if (i == rowPos and j == colPos):
+                canvas[i][j] = char
+                
+                a = 1
 
-#    print(canvas)
+                if (direction == 'h'):
+
+                    if (height > length + 1):
+                        length = length - 1
+
+                    for a in range(length):
+                        if (j in range(height - 1)):
+                            j += 1
+                            canvas[i][j] = char
+                    a = 0
+
+                if (direction == 'v'):
+
+                    if (width > length):
+                        length = length - 1
+
+                    for a in range(length):
+                        if (i in range(width - 1)):
+                            i += 1
+                            canvas[i][j] = char
+                    a = 0
+
+    return canvas
+
+def show(canvas):
+
+    print('\nFinal result:')
+    for i in range(width):
+        for j in range(height):
+            print(canvas[i][j], end=' ')
+        print()
 
 
 if __name__ == "__main__":
